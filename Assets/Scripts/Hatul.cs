f18453bf3a26974f8cfd34faa9b5b5704af65c75
orenkoren <oren-k@hotmail.com>
2021-06-12 10:37:14 +0300

    Hatul & Hatul Armageddon basic logic

---
+++ Assets/Scripts/Hatul.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeshMove : MonoBehaviour
+{
+    [SerializeField] private Transform _destination;
+    [SerializeField] private GameObject _hatulArmageddon;
+    private NavMeshAgent _navMeshAgent;
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.SqrMagnitude(_destination.transform.position - transform.position) < 3)
+        {
+            Instantiate(_hatulArmageddon, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
