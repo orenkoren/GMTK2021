87a348c93bfa3fe169ae508c1ae815b3151b5811
orenkoren <oren-k@hotmail.com>
2021-06-12 19:43:58 +0300

    Pause menu + Art

--- Assets/Scripts/GrandmaSpawner.cs
+++ Assets/Scripts/GrandmaSpawner.cs
@@ -22,7 +22,7 @@ public class GrandmaSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!PauseMenu.paused && Input.GetMouseButtonDown(0))
         {
             if (indicatorInstance == null || repositionIndicator())
             {
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject ui;
+    public static bool paused = false;
+    void Start()
+    {
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        ui.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void Pause()
+    {
+        ui.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void ChangeVolume(float value)
+    {
+        // AudioListener.volume = value;
+    }
+}
